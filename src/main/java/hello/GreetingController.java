dc1ff81d21899f3e50838ec80a367812c2373fac
sfiscus-nurocor <47430798+sfiscus-nurocor@users.noreply.github.com>
2019-08-05 10:38:22 -0500

    test update2

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -12,13 +12,13 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Test") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
 
     @RequestMapping("/")
-    public String defaultEndpoint(@RequestParam(value="name", defaultValue="Test") String name) {
+    public String defaultEndpoint(@RequestParam(value="name", defaultValue="World") String name) {
         return String.format(template, name);
     }
 }
